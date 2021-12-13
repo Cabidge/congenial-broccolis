04e70f86a7d9825f5c276a53a3f6e0883fed295a
Cabidge <cabbageboi69@gmail.com>
2021-12-12 20:17:24 -0500

    update files with basic setup

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,26 @@
 # SoftDev
 # P01 -- ArRESTed Development
 # 2021-12-10
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "home"
+
+
+@app.route("/login")
+def login():
+    return "login"
+
+
+@app.route("/signup")
+def signup():
+    return "signup"
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
